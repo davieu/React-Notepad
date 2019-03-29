cc3c45a7856645117d5981695b57f714e51e5f1e
davieu <davisumana@gmail.com>
2019-03-29 17:44:07 -0400

    added a state for currently selected note to display in input

--- src/components/Notes/NotesList.js
+++ src/components/Notes/NotesList.js
@@ -3,7 +3,8 @@ import React from 'react';
 const notesList = (props) => props.notes.map((note) => {
   return (
     <div key={note.id}>
-      <button onClick={() => props.selectNote(note.id)}>{note.id} {note.created} {note.note} </button>
+      {/* <button onClick={(event) => props.selectNote(note.id, event)}>{note.id} {note.dateCreated} {note.note} </button> */}
+      <button onClick={() => props.selectNote(note.id)}>{note.id} {note.dateCreated} {note.note} </button>
       <button onClick={() => props.clickDelete(note.id)}>X</button>
     </div>
   )
--- src/containers/App.js
+++ src/containers/App.js
@@ -9,52 +9,75 @@ let uniqid = require('uniqid');
 
 class App extends Component {
   state = {
-    notes: [{id: uniqid(), note: 'test1', created: moment().format('L')}, {id: uniqid(), note: 'test2', created: moment().format('L')}],
-    noteListShowing: true
-  }
+    notes: [{id: uniqid(), note: 'test1 asfdf dfg dfg sdg', dateCreated: moment().format('L')}, {id: uniqid(), note: 'test2 af afgerg erg asdd', dateCreated: moment().format('L')}],
+    noteListShowing: true,
+    currentNote: '',
+    currentlySelectedIndex: 0
+  };
 
   //toggle for hiding and showing the notes list.
   isNoteListShowing = () => {
     const isShowing = this.state.noteListShowing;
     this.setState({
       noteListShowing: !isShowing
-    })
-  }
+    });
+  };
 
   //creates a new note on the list
   addNewNote = () => {
-    const copyNotes = [...this.state.notes]
+    const copyNotes = [...this.state.notes];
     let newNote = {
       id: uniqid(),
       note: '',
-      created: moment().format('L'),
+      dateCreated: moment().format('L')
     };
 
     this.setState({
       notes: [...copyNotes, newNote]
-    })
-  }
+    });
+  };
 
+  //helper function for finding the selected note id/index
   selectedNoteID__Helper = (noteID) => {
     let indexSelected = this.state.notes.findIndex(note => {
       return note.id === noteID;
     })
-    return indexSelected
-  }
+    return indexSelected;
+  };
 
-  selectNote = (noteID) => {
-    let noteIndex = this.selectedNoteID__Helper(noteID)
-  }
+  // selectNoteForInput = (event, noteID) => {
+  //   let noteIndex = this.selectedNoteID__Helper(noteID);
+  //   const copyNote = {
+  //     ...this.state.notes[noteIndex]
+  //   };
+
+  //   copyNote.note = event.target.value;
+
+  //   const copyNotes = [...this.state.notes];
+  //   copyNotes[noteIndex] = copyNote;
+
+  //   this.setState({ notes: copyNotes })
+  //   console.log(noteIndex)
+  // };
+
+  selectNoteForInput = (noteID) => {
+    let noteIndex = this.selectedNoteID__Helper(noteID);
+    this.setState({
+      currentlySelectedIndex: noteIndex
+    })
+    console.log(noteIndex)
+  };
 
   deleteNoteFromList = (noteID) => {
-    const copyNotes = [...this.state.notes]
-    let noteIndex = this.selectedNoteID__Helper(noteID)
+    const copyNotes = [...this.state.notes];
+    let noteIndex = this.selectedNoteID__Helper(noteID);
 
-    copyNotes.splice(noteIndex, 1)
+    copyNotes.splice(noteIndex, 1);
+    
     this.setState({
       notes: copyNotes
-    })
-  }
+    });
+  };
 
   render() {
     let noteList = null;
@@ -62,10 +85,10 @@ class App extends Component {
     //toggle for showing the notes
     if (this.state.noteListShowing) {
       noteList = <NotesList 
-        selectNote={this.selectNote}
+        selectNote={this.selectNoteForInput}
         notes={this.state.notes}
-        clickDelete={this.deleteNoteFromList}/>
-    }
+        clickDelete={this.deleteNoteFromList}/>;
+    };
 
     // let notes = this.state.notes.map(el => {
     //   return <button key={el.id}>{el.note}  {el.created} {el.id}</button>
@@ -80,7 +103,8 @@ class App extends Component {
           <button onClick={this.addNewNote} className="add-Note">Add Note</button>
           <button onClick={this.isNoteListShowing} className="hide-list">{this.state.noteListShowing ? 'Hide' : 'Show'}</button>
         </div>
-        <input type="text" className="note-text" />
+        {/* <input type="text" className="note-text" onChange={this.selectNoteForInput} value={this.state.currentNote}/> */}
+        <input type="text" className="note-text" value={this.state.notes[this.state.currentlySelectedIndex].note}/>
         {noteList}
       </div>
     );
